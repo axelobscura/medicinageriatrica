c2e75d60adc44263bb9d5ffc010efd68146b9969
Axel Laurent <axosar@gmail.com>
2025-07-10 12:33:27 -0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useCategorias } from '../../lib/swr-hooks'
 
 export default function Home() {
-  const {categorias, isLoadingCategorias, isError} = useCategorias();
+  const {categorias, isLoadingCategorias} = useCategorias();
 
   if(isLoadingCategorias){
     return(
@@ -12,7 +12,6 @@ export default function Home() {
   }
 
   console.log(categorias);
-  console.log("Error: ", isError);
 
   return (
     <div className="grid grid-rows-[1fr] items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
